307d30718f44e23279a1b4ebdf67114a4a75388c
Maksim Krampit <savage_jawa@mail.ru>
2021-03-22 15:41:53 +0700

    HW5

--- RPG/Assets/Scripts/Character.cs
+++ RPG/Assets/Scripts/Character.cs
@@ -15,6 +15,12 @@ public class Character : MonoBehaviour
     private Animator _anim = null;
     private AudioSource _as = null;
     [SerializeField] private AudioClip[] ac = null;
+    private bool _ikActive = false;
+    private Transform _lookObj = null;
+    [SerializeField] private Transform _rightHand = null;
+    [SerializeField] private Transform _lefttHand = null;
+    private Transform _fence = null;
+    private GameObject _attackedEnemy = null;
 
     private void Awake()
     {
@@ -22,15 +28,78 @@ public class Character : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         _moveTarget = transform.position;
         _anim = GetComponent<Animator>();
-
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            _lookObj = other.transform;
+            _ikActive = true;
+        }
+        else if (other.CompareTag("Fence"))
+        {
+            _fence = other.transform;
+            _lookObj = other.transform;
+            _ikActive = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy") || other.CompareTag("Fence"))
+        {
+            _fence = null;
+            _lookObj = null;
+            _ikActive = false;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Init(_data);
-        
     }
 
+    private void OnAnimatorIK()
+    {
+       if (_anim)
+        {
+
+            if (_ikActive)
+            {
+                if (_lookObj != null)
+                {
+                    _anim.SetLookAtWeight(1);
+                    _anim.SetLookAtPosition(_lookObj.position + new Vector3(0, 3, 0));
+                }
+                if (_fence!=null)
+                {
+                    _anim.SetLookAtWeight(0);
+                    if ((_lookObj.position - _rightHand.position).magnitude > (_lookObj.position - _lefttHand.position).magnitude)
+                    {
+                        _anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                        _anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+                        _anim.SetIKPosition(AvatarIKGoal.LeftHand, _lefttHand.position);
+                        _anim.SetIKRotation(AvatarIKGoal.LeftHand, _lefttHand.rotation);
+                    }
+                        else 
+                            {
+                                _anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                                _anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                                _anim.SetIKPosition(AvatarIKGoal.RightHand, _rightHand.position);
+                                _anim.SetIKRotation(AvatarIKGoal.RightHand, _rightHand.rotation); 
+                            }
+                }
+            }
+            else
+            {
+                _anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 0);
+                _anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 0);
+                _anim.SetLookAtWeight(0);
+            }
+        }
+    }
 
     private void OnGUI()
     {
@@ -63,8 +132,25 @@ public class Character : MonoBehaviour
                 _moveTarget = hit.point+new Vector3(0,1,0);
                 transform.LookAt(_moveTarget);
             }
+            else if (hit.collider.gameObject.CompareTag("Enemy") && (hit.collider.gameObject.transform.position-gameObject.transform.position).magnitude<4)
+            {
+                transform.LookAt(hit.collider.gameObject.transform);
+                int a = Random.Range(0, 3);
+                Debug.Log(a);
+                if (a == 0) _anim.SetTrigger("attack");
+                else if (a == 1) _anim.SetTrigger("kick");
+                else _anim.SetTrigger("punch");
+               _attackedEnemy =  hit.collider.gameObject;
+            }
+            else if (hit.collider.gameObject.CompareTag("Enemy") && (hit.collider.gameObject.transform.position - gameObject.transform.position).magnitude > 4)
+            {
+                transform.LookAt(hit.collider.gameObject.transform);
+                _moveTarget = hit.collider.gameObject.transform.position;
+            }
+
         }
-        if ((transform.position - _moveTarget).magnitude >= 1)
+
+        if ((transform.position - _moveTarget).magnitude >= 2)
         {
             _anim.SetBool("isWalking", true);
             rb.AddForceAtPosition(Vector3.ClampMagnitude(((_moveTarget-transform.position)*0.5f), 5f), _moveTarget, ForceMode.VelocityChange);
@@ -102,18 +188,21 @@ public class Character : MonoBehaviour
         _as.PlayOneShot(ac[0]);
     }
 
-    public void Kick()
+    public void Kick(GameObject enemy)
     {
+        _attackedEnemy.GetComponent<Skeleton>().Death();
         _as.PlayOneShot(ac[2]);
     }
 
     public void Punch()
     {
+        _attackedEnemy.GetComponent<Skeleton>().Death();
         _as.PlayOneShot(ac[1]);
     }
 
     public void Axe()
     {
+        _attackedEnemy.GetComponent<Skeleton>().Death();
         _as.PlayOneShot(ac[3]);
     }
 
---
+++ RPG/Assets/Scripts/Skeleton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+    [SerializeField] private List<Rigidbody> _partsOfBody = null;
+    private Animator _anim = null;
+    private Rigidbody _rb = null;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+        _partsOfBody.AddRange(GetComponentsInChildren<Rigidbody>());
+        isKinematicOn();
+    }
+
+    private void isKinematicOn()
+    {
+        _anim.enabled = true;
+        foreach (var obj in _partsOfBody)
+        {
+            obj.isKinematic = true;
+        }
+    }
+
+    public void Death()
+    {
+        _anim.enabled = false;
+        foreach (var obj in _partsOfBody)
+        {
+            obj.isKinematic = false;
+        }
+        _rb.AddForce((gameObject.transform.position - GameObject.FindGameObjectWithTag("Player").transform.position).normalized * 15, ForceMode.Impulse);
+    }
+}
